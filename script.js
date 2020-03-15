99b6f1e7758bc3c7054554ba14883089929fd741
Olga Bainova (nb) <terra.456@yandex.ru>
2020-03-15 23:48:39 +0500

    add script.js and refactor styles of header

---
+++ script.js
@@ -0,0 +1,9 @@
+const MENU = document.querySelector('.navigation__list');
+MENU.querySelectorAll('a');
+MENU.addEventListener('click', (event) => {
+    MENU.querySelectorAll('a').forEach;
+    MENU.classList.add('.navigation__item--current');
+    MENU.classList.remove('.navigation__item--current');
+    console.log(1);
+}
+);
